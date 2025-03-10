e1585566e98748732adcae4575c5c1c1d15c5cbd
Richmond Antor <antorgm2@gmail.com>
2025-03-10 12:51:48 +0600

    Add files via upload

---
+++ teRNary.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+// ternary operator ?: = replace to if else statement
+//condition ? expression1 : expression2;
+
+int main(){
+int grade = 75;
+
+grade >= 60 ? std::cout<<"you pass!" :  std::cout<<"you pass!";
+
+
+
+
+return 0;
+}
