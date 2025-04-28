ffb9091a2eddd130c7929d41d8ff5ff90ffe14e1
Richmond Antor <antorgm2@gmail.com>
2025-04-29 01:07:18 +0600

    Add files via upload

---
+++ Calorie Intake.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int x,y,z;
+cin>>x>>y>>z;
+
+if(x>=(x*y)){
+
+    cout<<x-y*z;
+}
+else{
+    cout<<-1;
+}
+
+
+
+}
---
+++ Minimum Wage.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+int x;
+cin>>x;
+if(x>11){
+    cout<<"Yes";
+}
+else{
+    cout<<"no";
+}
+
+}
+
+
