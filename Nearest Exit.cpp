70f4ae314b1742be667a84264a053eb1348af677
Richmond Antor <antorgm2@gmail.com>
2025-07-01 23:42:03 +0600

    Create Nearest Exit.cpp

---
+++ Nearest Exit.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t,x;
+	cin>>t;
+	while(t--){
+	    cin>>x;
+	    if(x<51){
+	        std::cout << "LEFT" << std::endl;
+	        
+	    }
+	    else{
+	        std::cout << "RIGHT" << std::endl;
+	    }
+	    
+	    
+	    
+	}
+
+}
