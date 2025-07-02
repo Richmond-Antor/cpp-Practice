f0c675cf8b52f22bfe36457959be3e1a3cf188dc
Richmond Antor <antorgm2@gmail.com>
2025-07-02 11:58:52 +0600

    Create Qualify the round.cpp

---
+++ Qualify the round.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t,x,a,b;
+	std::cin >> t;
+	while(t--){
+	    cin>>x>>a>>b;
+	    if (x>(a+b*2)){
+	        cout<<"NotQualify"<<endl;
+	        
+	    }
+	    else{
+	       cout<<"Qualify"<<endl;
+	    }
+	    
+	    
+	}
+
+}
