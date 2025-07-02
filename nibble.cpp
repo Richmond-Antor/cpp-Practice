4480090cb21d9d2ce0d5a9b6d318038bcb1121f3
Richmond Antor <antorgm2@gmail.com>
2025-07-02 11:41:43 +0600

    Create nibble.cpp

---
+++ nibble.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t,n;
+	std::cin >> t;
+	while(t--){
+	    cin>>n;
+	    if(n%4==0){
+	        std::cout << "Good" << std::endl;
+	    }
+	    else{
+	        cout<<"Not Good" << endl;
+	    }
+	}
+
+}
