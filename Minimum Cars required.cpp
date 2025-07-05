1e4d88c00e43fb75ac14e100459e18fb8a227934
Richmond Antor <antorgm2@gmail.com>
2025-07-05 23:07:24 +0600

    Create Minimum Cars required.cpp

---
+++ Minimum Cars required.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t, n;
+	cin>>t;
+	while(t--){
+	    
+	    cin>>n;
+	    if(n%4==0){
+	        
+	        cout<<n/4<<endl;
+	        
+	    }
+	    else{
+	        cout<<(n/4)+1<<endl;
+	    }
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	}
+
+}
